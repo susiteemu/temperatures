6ab06d39fb66093fbb300dc92c9704eb95f7348b
Teemu Turunen <teemu@teemuturunen.net>
2024-11-19 16:52:43 +0200

    Change infoscreen layout

--- infoscreen-img-gen/imagedraw.go
+++ infoscreen-img-gen/imagedraw.go
@@ -45,6 +45,7 @@ func drawResult(measurements []Measurement, imageConfiguration *GenerateImageCon
 	imgWidth := imageConfiguration.ImgW
 	imgHeight := imageConfiguration.ImgH
 	fontSizeL := imageConfiguration.FontL
+	fontSizeLMinus := imageConfiguration.FontL - 18
 	fontSizeM := imageConfiguration.FontM
 	fontSizeS := imageConfiguration.FontS
 
@@ -65,6 +66,7 @@ func drawResult(measurements []Measurement, imageConfiguration *GenerateImageCon
 	draw.Draw(rgba, rgba.Bounds(), bg, image.Point{0, 0}, draw.Src)
 
 	defaultFontHeight := int(math.Ceil(fontSizeL * spacing * dpi / 72))
+	defaultHalfCellFontHeight := int(math.Ceil(fontSizeLMinus * spacing * dpi / 72))
 	labelFontHeight := int(math.Ceil(fontSizeM * spacing * dpi / 72))
 	smallFontHeight := int(math.Ceil(fontSizeS * spacing * dpi / 72))
 
@@ -85,6 +87,16 @@ func drawResult(measurements []Measurement, imageConfiguration *GenerateImageCon
 		Src: fg,
 		Face: truetype.NewFace(defaultFont, &truetype.Options{
 			Size:    fontSizeL,
+			DPI:     dpi,
+			Hinting: h,
+		}),
+	}
+
+	halfCellDefaultDrawer := &font.Drawer{
+		Dst: rgba,
+		Src: fg,
+		Face: truetype.NewFace(defaultFont, &truetype.Options{
+			Size:    fontSizeLMinus,
 			DPI:     dpi,
 			Hinting: h,
 		}),
@@ -113,71 +125,86 @@ func drawResult(measurements []Measurement, imageConfiguration *GenerateImageCon
 	rows := len(measurements) / cols
 	rowHeight := imgHeight / rows
 	colWidth := imgWidth / cols
-	for idx, m := range measurements {
-		// calculate center for grid
-		cX := colWidth / 2
-		cY := rowHeight*(idx/2) + (rowHeight / 2) - (labelFontHeight / 2) - (defaultFontHeight / 2)
-		if (idx+1)%2 == 0 {
-			cX = colWidth + colWidth/2
+
+	const (
+		NORMAL_CELL = 0
+		DOUBLE_CELL = 1
+		HALF_CELL   = 2
+		LEFT_SIDE   = 0
+		RIGHT_SIDE  = 1
+	)
+	// indexing starts from top left, does down the left side then continues from top left and goes down
+	indexTypes := []int{
+		DOUBLE_CELL, NORMAL_CELL, NORMAL_CELL, NORMAL_CELL, HALF_CELL, HALF_CELL,
+	}
+	currentSide := LEFT_SIDE
+	currentY := 0
+	for idx := 0; idx < len(measurements); idx++ {
+
+		m := measurements[idx]
+		cellType := indexTypes[idx]
+
+		switch cellType {
+		case NORMAL_CELL:
+			cellX := 0
+			cellY := currentY
+			cellW := colWidth
+			cellH := rowHeight
+			switch currentSide {
+			case LEFT_SIDE:
+				cellX = 0
+			case RIGHT_SIDE:
+				cellX = colWidth
+			}
+
+			drawCell(m, cellX, cellY, cellW, cellH, labelFontHeight, defaultFontHeight, smallFontHeight, labelDrawer, defaultDrawer, smallDrawer, whiteSmallDrawer, rgba, fg)
+
+			currentY += cellH
+		case DOUBLE_CELL:
+			cellX := 0
+			cellY := currentY
+			cellW := colWidth
+			cellH := (imgHeight / 3) * 2
+			switch currentSide {
+			case LEFT_SIDE:
+				cellX = 0
+			case RIGHT_SIDE:
+				cellX = colWidth
+			}
+
+			drawDoubleCell(m, cellX, cellY, cellW, cellH, labelFontHeight, defaultFontHeight, smallFontHeight, labelDrawer, defaultDrawer, smallDrawer, whiteSmallDrawer, rgba, fg)
+
+			currentY += cellH
+		case HALF_CELL:
+			cellX := 0
+			cellY := currentY
+			cellW := colWidth / 2
+			cellH := rowHeight
+			switch currentSide {
+			case LEFT_SIDE:
+				cellX = 0
+			case RIGHT_SIDE:
+				cellX = colWidth
+			}
+
+			drawCell(m, cellX, cellY, cellW, cellH, labelFontHeight, defaultHalfCellFontHeight, smallFontHeight, labelDrawer, halfCellDefaultDrawer, smallDrawer, whiteSmallDrawer, rgba, fg)
+
+			if idx+1 < len(measurements) && indexTypes[idx+1] == HALF_CELL {
+				idx++
+				nextM := measurements[idx]
+				cellX += colWidth / 2
+
+				drawCell(nextM, cellX, cellY, cellW, cellH, labelFontHeight, defaultHalfCellFontHeight, smallFontHeight, labelDrawer, halfCellDefaultDrawer, smallDrawer, whiteSmallDrawer, rgba, fg)
+
+			}
+
+			currentY += cellH
+
 		}
 
-		label := m.FormatLabel()
-		labelDrawer.Dot = fixed.Point26_6{
-			X: fixed.I(cX) - labelDrawer.MeasureString(label)/2,
-			Y: fixed.I(cY),
-		}
-		labelDrawer.DrawString(label)
-
-		val := m.FormatValue()
-		defaultDrawer.Dot = fixed.Point26_6{
-			X: fixed.I(cX) - defaultDrawer.MeasureString(val)/2,
-			Y: fixed.I(cY + defaultFontHeight),
-		}
-		defaultDrawer.DrawString(val)
-
-		if !m.Empty {
-			log.Debug().Msg("Drawing other things")
-			labelDrawer.Dot = fixed.Point26_6{
-				X: fixed.I(cX) + defaultDrawer.MeasureString(val)/2,
-				Y: fixed.I(cY + (defaultFontHeight / 2)),
-			}
-			labelDrawer.DrawString("°C")
-
-			margin := 8
-			nY := int(idx/2)*rowHeight + rowHeight - margin
-			log.Debug().Msgf("At %d calculated modulus %d and got nY %d", idx, (idx+1)%2, nY)
-
-			nX := colWidth - margin
-			if (idx+1)%2 == 0 {
-				nX = colWidth + colWidth - margin
-			}
-
-			ageWidth := smallDrawer.MeasureString(m.FormatAge())
-			if ageWidth.Ceil() > 1 {
-				rect := image.Rect(nX-ageWidth.Ceil()-margin, nY-smallFontHeight, nX+margin, nY+margin)
-				draw.Draw(rgba, rect, fg, image.Point{0, 0}, draw.Src)
-
-				whiteSmallDrawer.Dot = fixed.Point26_6{
-					X: fixed.I(nX) - ageWidth,
-					Y: fixed.I(nY),
-				}
-				whiteSmallDrawer.DrawString(m.FormatAge())
-
-				nX = nX - ageWidth.Ceil() - margin - (margin + 1)
-			}
-
-			slopeWidth := smallDrawer.MeasureString(m.FormatSlope())
-			if slopeWidth.Ceil() > 1 {
-				rect := image.Rect(nX-slopeWidth.Ceil()-margin, nY-smallFontHeight, nX+margin, nY+margin)
-				draw.Draw(rgba, rect, fg, image.Point{0, 0}, draw.Src)
-
-				whiteSmallDrawer.Dot = fixed.Point26_6{
-					X: fixed.I(nX) - slopeWidth,
-					Y: fixed.I(nY),
-				}
-				whiteSmallDrawer.DrawString(m.FormatSlope())
-			}
-
+		if currentSide != RIGHT_SIDE && currentY >= imgHeight {
+			currentSide = RIGHT_SIDE
+			currentY = 0
 		}
 
 	}
@@ -185,10 +212,24 @@ func drawResult(measurements []Measurement, imageConfiguration *GenerateImageCon
 	for i := 0; i < imgHeight; i++ {
 		rgba.Set(imgWidth/2, i, ruler)
 	}
+	// Left side is divided to two rows: 2/3 and 1/3
+	leftSideDivideIdx := 2
+	leftSideStop := imgWidth / 2
+	for x := 0; x < leftSideStop; x++ {
+		rgba.Set(x, leftSideDivideIdx*rowHeight, ruler)
+	}
+	// Right side is divided to 3x1/3 rows
+	rightSideStart := imgWidth / 2
 	for y := 0; y < rows; y++ {
-		for x := 0; x < imgWidth; x++ {
+		for x := rightSideStart; x < imgWidth; x++ {
 			rgba.Set(x, y*rowHeight, ruler)
 		}
+	}
+	// Right side bottom cell is divided to two equal parts
+	rightBottomCellX := (imgWidth / 4) * 3
+	rightBottomCellY := (imgHeight / 3) * 2
+	for i := rightBottomCellY; i < imgHeight; i++ {
+		rgba.Set(rightBottomCellX, i, ruler)
 	}
 
 	uX := 5
@@ -234,4 +275,126 @@ func drawResult(measurements []Measurement, imageConfiguration *GenerateImageCon
 		log.Error().Err(err).Msg("Failed to run 'convert' command")
 	}
 	log.Info().Msg("Successfully wrote image")
+}
+
+func drawCell(m Measurement, x, y, w, h, lfh, dfh, sfh int, ld, dd, sd, wsd *font.Drawer, rgba *image.RGBA, fg *image.Uniform) {
+
+	// calculate center for grid
+	cX := x + (w / 2)
+	cY := y + (h / 2) - (lfh / 2) - (dfh / 2)
+
+	label := m.FormatLabel()
+	ld.Dot = fixed.Point26_6{
+		X: fixed.I(cX) - ld.MeasureString(label)/2,
+		Y: fixed.I(cY),
+	}
+	ld.DrawString(label)
+
+	val := m.FormatValue()
+	dd.Dot = fixed.Point26_6{
+		X: fixed.I(cX) - dd.MeasureString(val)/2,
+		Y: fixed.I(cY + dfh),
+	}
+	dd.DrawString(val)
+
+	if !m.Empty {
+		log.Debug().Msg("Drawing other things")
+		ld.Dot = fixed.Point26_6{
+			X: fixed.I(cX) + dd.MeasureString(val)/2,
+			Y: fixed.I(cY + (dfh / 2)),
+		}
+		ld.DrawString("°C")
+
+		margin := 8
+		nY := y + h - margin
+		nX := x + w - margin
+
+		ageWidth := sd.MeasureString(m.FormatAge())
+		if ageWidth.Ceil() > 1 {
+			rect := image.Rect(nX-ageWidth.Ceil()-margin, nY-sfh, nX+margin, nY+margin)
+			draw.Draw(rgba, rect, fg, image.Point{0, 0}, draw.Src)
+
+			wsd.Dot = fixed.Point26_6{
+				X: fixed.I(nX) - ageWidth,
+				Y: fixed.I(nY),
+			}
+			wsd.DrawString(m.FormatAge())
+
+			nX = nX - ageWidth.Ceil() - margin - (margin + 1)
+		}
+
+		slopeWidth := sd.MeasureString(m.FormatSlope())
+		if slopeWidth.Ceil() > 1 {
+			rect := image.Rect(nX-slopeWidth.Ceil()-margin, nY-sfh, nX+margin, nY+margin)
+			draw.Draw(rgba, rect, fg, image.Point{0, 0}, draw.Src)
+
+			wsd.Dot = fixed.Point26_6{
+				X: fixed.I(nX) - slopeWidth,
+				Y: fixed.I(nY),
+			}
+			wsd.DrawString(m.FormatSlope())
+		}
+
+	}
+}
+
+func drawDoubleCell(m Measurement, x, y, w, h, lfh, dfh, sfh int, ld, dd, sd, wsd *font.Drawer, rgba *image.RGBA, fg *image.Uniform) {
+
+	// calculate center for grid
+	cX := x + (w / 2)
+	cY := y + (h / 4) - (lfh / 2) - (dfh / 2)
+
+	label := m.FormatLabel()
+	ld.Dot = fixed.Point26_6{
+		X: fixed.I(cX) - ld.MeasureString(label)/2,
+		Y: fixed.I(cY),
+	}
+	ld.DrawString(label)
+
+	val := m.FormatValue()
+	dd.Dot = fixed.Point26_6{
+		X: fixed.I(cX) - dd.MeasureString(val)/2,
+		Y: fixed.I(cY + dfh),
+	}
+	dd.DrawString(val)
+
+	if !m.Empty {
+		log.Debug().Msg("Drawing other things")
+		ld.Dot = fixed.Point26_6{
+			X: fixed.I(cX) + dd.MeasureString(val)/2,
+			Y: fixed.I(cY + (dfh / 2)),
+		}
+		ld.DrawString("°C")
+
+		margin := 8
+		nY := y + h - margin
+		nX := x + w - margin
+
+		ageWidth := sd.MeasureString(m.FormatAge())
+		if ageWidth.Ceil() > 1 {
+			rect := image.Rect(nX-ageWidth.Ceil()-margin, nY-sfh, nX+margin, nY+margin)
+			draw.Draw(rgba, rect, fg, image.Point{0, 0}, draw.Src)
+
+			wsd.Dot = fixed.Point26_6{
+				X: fixed.I(nX) - ageWidth,
+				Y: fixed.I(nY),
+			}
+			wsd.DrawString(m.FormatAge())
+
+			nX = nX - ageWidth.Ceil() - margin - (margin + 1)
+		}
+
+		slopeWidth := sd.MeasureString(m.FormatSlope())
+		if slopeWidth.Ceil() > 1 {
+			rect := image.Rect(nX-slopeWidth.Ceil()-margin, nY-sfh, nX+margin, nY+margin)
+			draw.Draw(rgba, rect, fg, image.Point{0, 0}, draw.Src)
+
+			wsd.Dot = fixed.Point26_6{
+				X: fixed.I(nX) - slopeWidth,
+				Y: fixed.I(nY),
+			}
+			wsd.DrawString(m.FormatSlope())
+		}
+
+	}
 }
