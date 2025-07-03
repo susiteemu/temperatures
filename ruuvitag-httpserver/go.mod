45f1eac529b402cf77e3bd4b1018b9c6f186518b
Teemu Turunen <teemu@teemuturunen.net>
2025-07-03 13:32:59 +0300

    Use go-jet with db queries; upgrade dependencies

--- ruuvitag-httpserver/go.mod
+++ ruuvitag-httpserver/go.mod
@@ -3,27 +3,29 @@ module ruuvitag-httpserver
 go 1.23.0
 
 require (
-	github.com/jackc/pgx/v5 v5.7.1
+	github.com/go-jet/jet/v2 v2.13.0
 	github.com/joho/godotenv v1.5.1
-	github.com/labstack/echo v3.3.10+incompatible
-	github.com/rs/zerolog v1.33.0
+	github.com/labstack/echo/v4 v4.13.4
+	github.com/rs/zerolog v1.34.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/rogpeppe/go-internal v1.6.1 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.28.0 // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.26.0 // indirect
-	golang.org/x/text v0.19.0 // indirect
+	golang.org/x/crypto v0.39.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- ruuvitag-httpserver/main.go
+++ ruuvitag-httpserver/main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
 	"os"
 	"strings"
 	"time"
 
+	"ruuvitag-httpserver/.gen/ruuvi/public/model"
+	. "ruuvitag-httpserver/.gen/ruuvi/public/table"
+
+	. "github.com/go-jet/jet/v2/postgres"
+
 	"github.com/joho/godotenv"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog/log"
 
-	"github.com/jackc/pgx/v5"
 	"gopkg.in/yaml.v2"
 )
 
-type Measurement struct {
+type MeasurementJson struct {
 	MAC                       string  `json:"mac"`
 	Temperature               float64 `json:"temp"`
 	Humidity                  float64 `json:"humidity"`
@@ -34,9 +38,10 @@ type Measurement struct {
 const CONFIG_PATH = "config.yml"
 
 var (
+	db            *sql.DB
 	configuration = map[string]string{}
 	envFile       = map[string]string{}
-	devices       = map[string]int{}
+	devices       = map[string]int64{}
 )
 
 func loadConfiguration() {
@@ -65,15 +70,25 @@ func loadConfiguration() {
 func main() {
 	loadConfiguration()
 
+	connectString := envFile["POSTGRESQL_CONN_URL"]
+	db, err := sql.Open("postgres", connectString)
+	if err != nil {
+		panic(err)
+	}
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(30 * time.Minute)
+	defer db.Close()
+
 	postMeasurement := func(c echo.Context) error {
-		m := new(Measurement)
+		m := new(MeasurementJson)
 		if err := c.Bind(m); err != nil {
 			log.Error().Err(err).Msgf("Failed to bind payload into measurement")
 			return echo.NewHTTPError(400, "Invalid data")
 		}
 		log.Info().Msgf("Received new measurement: %v", m)
 
-		err := writeToPostgres(m)
+		err := writeToPostgresWithJet(m)
 		if err != nil {
 			log.Error().Err(err).Msgf("Failed to write to Influxdb")
 			return echo.NewHTTPError(500, "Failed to write data")
@@ -88,64 +103,56 @@ func main() {
 	e.Use(middleware.Logger())
 	e.POST("/measurements", postMeasurement)
 	e.Logger.Fatal(e.Start(":1323"))
-
 }
 
-func writeToPostgres(m *Measurement) error {
-
-	log.Debug().Msg("Writing to Posgresql...")
-
-	connUrl := envFile["POSTGRESQL_CONN_URL"]
-	conn, err := pgx.Connect(context.Background(), connUrl)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
+func writeToPostgresWithJet(m *MeasurementJson) error {
+	var err error
 
 	if len(devices) == 0 {
-		rows, err := conn.Query(context.Background(), "select id, mac, label from device")
-		if err != nil {
-			log.Error().Err(err).Msg("Failed to query all devices")
-			return err
+		stmt := SELECT(Device.ID, Device.Mac).FROM(Device)
+		var allDevices []struct {
+			model.Device
 		}
-
-		for rows.Next() {
-			var (
-				id    int
-				mac   string
-				label string
-			)
-			rows.Scan(&id, &mac, &label)
-			log.Debug().Msgf("id=%d, mac=%s, label=%s\n", id, mac, label)
-			devices[strings.ToLower(mac)] = id
+		stmt.Query(db, &allDevices)
+		for _, device := range allDevices {
+			devices[strings.ToLower(device.Mac)] = int64(device.ID)
 		}
 	}
 
 	deviceId, has := devices[strings.ToLower(m.MAC)]
 	if !has {
 		log.Warn().Msgf("Unknown mac %s, skipping writing data to Postgresql", m.MAC)
-		return fmt.Errorf("Unknown mac %s, skipping writing data to Postgresql", m.MAC)
+		return fmt.Errorf("unknown mac %s, skipping writing data to Postgresql", m.MAC)
 	}
-
 	createdAt := time.Now().Truncate(time.Minute)
-	var measurementId int
-	err = conn.QueryRow(context.Background(), "select id from measurement where device_id=$1 and created_at=$2", deviceId, createdAt).Scan(&measurementId)
+	var measurementId int64
+
+	selectMeasurementStmt := SELECT(Measurement.ID).FROM(Measurement).WHERE(Measurement.DeviceID.EQ(Int(deviceId)).AND(Measurement.CreatedAt.EQ(TimestampzT(createdAt))))
+
+	err = selectMeasurementStmt.Query(db, &measurementId)
 	if err != nil {
 		measurementId = -1
 		log.Error().Err(err).Msgf("Failed to query device %d measurement at %v", deviceId, createdAt)
 	}
 
 	if measurementId == -1 {
-		_, err = conn.Exec(context.Background(), "insert into measurement (device_id, created_at, temperature, humidity, pressure, acceleration_x, acceleration_y, acceleration_z, battery_voltage, tx_power, movement_counter, measurement_sequence_number, rssi) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)", deviceId, createdAt, m.Temperature, m.Humidity, m.Pressure, m.AccelerationX, m.AccelerationY, m.AccelerationZ, m.Battery, m.TxPower, m.MovementCounter, m.MeasurementSequenceNumber, m.Rssi)
+		insertStmt := Measurement.
+			INSERT(Measurement.DeviceID, Measurement.CreatedAt, Measurement.Temperature, Measurement.Humidity, Measurement.Pressure, Measurement.AccelerationX, Measurement.AccelerationY, Measurement.AccelerationZ, Measurement.BatteryVoltage, Measurement.TxPower, Measurement.MovementCounter, Measurement.MeasurementSequenceNumber, Measurement.Rssi).
+			VALUES(deviceId, createdAt, m.Temperature, m.Humidity, m.Pressure, m.AccelerationX, m.AccelerationY, m.AccelerationZ, m.Battery, m.TxPower, m.MovementCounter, m.MeasurementSequenceNumber, m.Rssi)
+
+		_, err = insertStmt.Exec(db)
 	} else {
-		_, err = conn.Exec(context.Background(), "update measurement set device_id=$1, created_at=$2, temperature=$3, humidity=$4, pressure=$5, acceleration_x=$6, acceleration_y=$7, acceleration_z=$8, battery_voltage=$9, tx_power=$10, movement_counter=$11, measurement_sequence_number=$12, rssi=$13 where id=$14", deviceId, createdAt, m.Temperature, m.Humidity, m.Pressure, m.AccelerationX, m.AccelerationY, m.AccelerationZ, m.Battery, m.TxPower, m.MovementCounter, m.MeasurementSequenceNumber, m.Rssi, measurementId)
+		updateStmt := Measurement.
+			UPDATE(Measurement.DeviceID, Measurement.CreatedAt, Measurement.Temperature, Measurement.Humidity, Measurement.Pressure, Measurement.AccelerationX, Measurement.AccelerationY, Measurement.AccelerationZ, Measurement.BatteryVoltage, Measurement.TxPower, Measurement.MovementCounter, Measurement.MeasurementSequenceNumber, Measurement.Rssi).
+			SET(deviceId, createdAt, m.Temperature, m.Humidity, m.Pressure, m.AccelerationX, m.AccelerationY, m.AccelerationZ, m.Battery, m.TxPower, m.MovementCounter, m.MeasurementSequenceNumber, m.Rssi).
+			WHERE(Measurement.ID.EQ(Int(measurementId)))
+
+		_, err = updateStmt.Exec(db)
 	}
 
 	if err != nil {
-		log.Error().Err(err).Msgf("Failed to insert data for device %d", deviceId)
+		log.Error().Err(err).Msgf("Failed to insert or update data for device %d", deviceId)
 		return err
 	}
-
 	return nil
 }
